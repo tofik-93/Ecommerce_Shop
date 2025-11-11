a60725a19b28507b30d493346ad2b478f44a300d
tofik-93 <tofikmifta87@gmail.com>
2025-11-11 08:59:01 +0300

    chore(postcss): switch to @tailwindcss/postcss and update Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true
-	},
-	images: {
-		remotePatterns: [
-			{ protocol: 'https', hostname: 'i.dummyjson.com' },
-			{ protocol: 'https', hostname: 'cdn.dummyjson.com' }
-		]
-	}
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		typedRoutes: true
-	},
+	typedRoutes: true,
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'i.dummyjson.com' },
